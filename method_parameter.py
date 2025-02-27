7e461b826915c6648422c86b90f6a8f2a4a71244
Chandra13July <code13july@gmail.com>
2025-02-27 13:17:34 +0700

    Kode Method Parameter

---
+++ method_parameter.py
@@ -0,0 +1,9 @@
+# Belajar Method dengan Parameter
+
+def say_hello(first_name, last_name):
+    """Fungsi untuk menyapa dengan nama depan dan belakang."""
+    print(f"Hello, {first_name} {last_name}!")
+
+# Memanggil fungsi dengan berbagai nama
+say_hello("Alief", "Chandra")
+say_hello("Mary", "Cantika")
