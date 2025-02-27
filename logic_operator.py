be61fc3f751d871639143c94d682e3a3a1c10d29
Chandra13July <code13july@gmail.com>
2025-02-27 09:24:53 +0700

    Kode Logic Operator

---
+++ logic_operator.py
@@ -0,0 +1,15 @@
+# Operator Logika
+
+# Operator AND
+print("AND Operator:")
+print(True and True)   # True
+print(True and False)  # False
+print(False and False) # False
+print(False and True)  # False
+
+# Operator OR
+print("\nOR Operator:")
+print(True or True)    # True
+print(True or False)   # True
+print(False or False)  # False
+print(False or True)   # True
