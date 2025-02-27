82ecd16c6ef32ba552e0b5647565b6d500feaf56
Chandra13July <code13july@gmail.com>
2025-02-27 08:36:51 +0700

    Kode Math

---
+++ math.py
@@ -0,0 +1,14 @@
+# Tambah  
+print(1 + 1)  # Hasil: 2  
+
+# Kurang  
+print(10 - 5)  # Hasil: 5  
+
+# Bagi  
+print(10 / 5)  # Hasil: 2.0  
+
+# Kali  
+print(10 * 5)  # Hasil: 50  
+
+# Sisa Bagi (Modulo)  
+print(10 % 3)  # Hasil: 1  
