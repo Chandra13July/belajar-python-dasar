60a0d27cce348232052b92ce166f58d82bce2df4
Chandra13July <code13july@gmail.com>
2025-02-27 08:55:14 +0700

    Kode String

---
+++ string.py
@@ -0,0 +1,14 @@
+# Belajar String
+
+# Data diri
+name = "Alief Chandra Darmawan"
+kota = "Jember"
+alamat = "Sukowono"
+
+# Menggabungkan nama depan dan belakang
+nama_depan = "Alief"
+nama_belakang = "Darmawan"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+# Cetak nama lengkap
+print(nama_lengkap)
