814766f44b22911c03f93fa888f68015bfb6fdab
Chandra13July <code13july@gmail.com>
2025-02-27 09:11:47 +0700

    Kode Input Number

---
+++ input_number.py
@@ -0,0 +1,13 @@
+# Belajar Input Number
+
+# Meminta input angka pertama
+a = int(input("Angka Pertama: "))
+
+# Meminta input angka kedua
+b = int(input("Angka Kedua: "))
+
+# Menghitung hasil penjumlahan
+hasil = a + b
+
+# Menampilkan hasil
+print(f"{a} + {b} = {hasil}")
