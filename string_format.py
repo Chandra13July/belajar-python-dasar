838c690df10c1d241be64d3caf8a6a217088ac98
Chandra13July <code13july@gmail.com>
2025-02-27 09:00:44 +0700

    Kode String Format

---
+++ string_format.py
@@ -0,0 +1,10 @@
+# Mendefinisikan nama
+nama_depan = "Alief"
+nama_tengah = "Chandra"
+nama_belakang = "Darmawan"
+
+# Menggabungkan nama dengan f-string
+sapa = f"Halo {nama_depan} {nama_tengah} {nama_belakang}"
+
+# Cetak sapaan
+print(sapa)
