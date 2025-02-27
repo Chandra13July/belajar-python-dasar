10d1695b5ab2a03c7f5312ddf40d0b88ad162281
Chandra13July <code13july@gmail.com>
2025-02-27 11:50:27 +0700

    Kode For Loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+# Belajar For Loop
+
+# Daftar pelanggan
+pelanggan = ["Alief", "Chandra", "Darmawan", "Mary"]
+
+# Menambahkan pelanggan baru
+pelanggan.extend(["Alex", "Raffi"])
+
+# Menampilkan semua nama pelanggan
+print("Daftar Pelanggan:")
+print("========================")
+for nama in pelanggan:
+    print(f"Nama Pelanggan : {nama}")
+    print("========================")
