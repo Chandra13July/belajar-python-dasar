e1063fda516c812be0299d91de038cc3b6f9ef7f
Chandra13July <code13july@gmail.com>
2025-02-27 11:03:20 +0700

    Kode If

---
+++ if.py
@@ -0,0 +1,15 @@
+# Belajar If-Statement
+
+# Jumlah orang dan zombie
+orang = 100
+zombie = 100
+
+# Logika kondisi
+if orang < zombie:
+    print("Kabur, dunia mau kiamat!")
+
+if orang > zombie:
+    print("Mari basmi zombie!")
+
+if orang == zombie:
+    print("Mari bersiap berperang!")
