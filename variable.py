c2eff1d0f57c32803700f497800642650b41dd43
Chandra13July <code13july@gmail.com>
2025-02-27 08:46:05 +0700

    Kode Variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+hello = "Halo Dunia"
+
+# Cetak variabel hello beberapa kali
+print(hello)
+print(hello)
+print(hello)
+
+# Ubah nilai variabel hello
+hello = "Hello Python"
+
+# Cetak variabel hello dengan nilai baru
+print(hello)
+print(hello)
